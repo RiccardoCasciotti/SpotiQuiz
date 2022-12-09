f839926bfb13eee95ceaa2cdd99defa396c14dc6
IvanC23 <crusco.ivan2@gmail.com>
2022-12-09 09:05:00 +0100

    Update user

---
+++ spotify_quiz/lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  final int uid;
+  String username;
+  int level;
+  int numberQuiz;
+  double experience;
+  int coins;
+
+  User({
+    required this.uid,
+    required this.username,
+    required this.level,
+    required this.numberQuiz,
+    required this.experience,
+    required this.coins,
+  });
+}
