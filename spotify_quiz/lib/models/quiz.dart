9a6bcf0afac2ca8187c1ec6bece66af3f5aee8b6
IvanC23 <crusco.ivan2@gmail.com>
2022-12-09 22:47:21 +0100

    Create quiz.dart

---
+++ spotify_quiz/lib/models/quiz.dart
@@ -0,0 +1,15 @@
+class Quiz {
+  final int uid;
+  final int points;
+  final int correct;
+  final int wrong;
+  final int typeOfQuiz;
+
+  Quiz({
+    required this.uid,
+    required this.points,
+    required this.correct,
+    required this.wrong,
+    required this.typeOfQuiz,
+  });
+}
